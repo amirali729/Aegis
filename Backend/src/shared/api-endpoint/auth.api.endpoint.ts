b016457d9c7f5a804a723b469044b9622cd13416
amirali729 <amirpechs10@gmail.com>
2026-07-22 15:30:02 +0500

    remove the user module

--- Backend/src/shared/api-endpoint/auth.api.endpoint.ts
+++ Backend/src/shared/api-endpoint/auth.api.endpoint.ts
@@ -1,3 +1,23 @@
+// | Endpoint                          | Verify JWT  | Priority     |
+// | --------------------------------- | ----------  | --------     |
+// | `GET /me`                         | ✅          | ⭐⭐⭐⭐⭐   |
+// | `PATCH /me`                       | ✅          | ⭐⭐⭐⭐     |
+// | `POST /verify-email`              | ❌          | ⭐⭐⭐⭐     |
+// | `POST /resend-verification-email` | ❌          | ⭐⭐⭐⭐     |
+// | `POST /forgot-password`           | ❌          | ⭐⭐⭐⭐⭐   |
+// | `POST /reset-password`            | ❌          | ⭐⭐⭐⭐⭐   |
+// | `DELETE /me`                      | ✅          | ⭐⭐⭐       |
+// | `POST /change-email`              | ✅          | ⭐⭐⭐       |
+
+// GET	/users	Get all users (Admin)
+// GET	/users/:id	Get a specific user
+// GET	/users/me	Get current user's profile
+// PATCH	/users/me	Update current user's profile
+// PATCH	/users/:id	Update any user (Admin)
+// DELETE	/users/me	Delete own account
+// DELETE	/users/:id	Delete user (Admin)
+
+
 const AuthApiEndpoint = {
     SIGNUP: '/auth/signup',
     LOGIN: '/auth/login',
